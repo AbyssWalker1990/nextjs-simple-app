dff0b94c83f21d8ebf4801b7dbc153b0f868b7ef
Vova <bordiian.v@gmail.com>
2023-04-10 21:12:19 +0300

    add Item conponent for displaying data from wiki

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -17,7 +17,7 @@ export default function Search() {
     className='w-50 flex justify-center md:justify-between' onSubmit={handleSubmit}>
       <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} className='bg-white p-2 w-80 text-xl rounded-xl' placeholder='Search'
       />
-      <button className='bg-white p-2 w-80 text-xl bg-slate-300 ml-2 font-bold'>
+      <button className='p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold'>
         🚀
       </button>
     </form>
